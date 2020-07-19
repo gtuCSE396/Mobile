cb4b352c3250a3a6949ad32b065b2d119169a9a8
ahmetberber <ahmet.berber2017@gtu.edu.tr>
2020-07-19 23:08:48 +0300

    Date and Time Scripts Added

---
+++ Assets/Scenes/Date.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Date : MonoBehaviour
+{
+
+    public GameObject theTime;
+    public int hour;
+    public int minutes;
+    public int seconds; 
+    
+    public GameObject theDate;
+
+    public int day;
+    public int month;
+    public int year;
+    void Update()
+    {
+        hour = System.DateTime.Now.Hour;
+        minutes = System.DateTime.Now.Minute;    
+        seconds = System.DateTime.Now.Second;
+        theTime.GetComponent<Text>().text = "TIME: " + hour + ":" + minutes + ":" + seconds; 
+
+        day = System.DateTime.Now.Day;
+        month = System.DateTime.Now.Month;
+        year = System.DateTime.Now.Year;
+        theDate.GetComponent<Text>().text = "DATE: " + day + ":" + month + ":" + year; 
+    }
+
+}
