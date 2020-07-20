3aa6baef3226bfef2b6d336c83d9ac56f584e834
Muhammetalp <mamikalp61@gmail.com>
2020-07-20 11:53:09 -0700

    Main Page (Possibly tentative) has been added.

---
+++ Assets/Scripts/mainpage_button_on_click.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class mainpage_button_on_click : MonoBehaviour
+{
+    public Button main_button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button but = main_button.GetComponent<Button>();
+        but.onClick.AddListener(task);
+    }
+
+    void task()
+    {
+        SceneManager.LoadScene("Start");
+    }
+
+
+}
