28cac84d41faabc6f27ac41e0e4356413455a3ec
Muhammetalp <mamikalp61@gmail.com>
2020-08-04 22:03:24 +0300

    Z-Graphic and Logs

--- Assets/Scripts/Window_Graph.cs
+++ Assets/Scripts/Window_Graph.cs
@@ -8,46 +8,133 @@ using CodeMonkey.Utils;
 public class Window_Graph : MonoBehaviour {
 
     [SerializeField] private Sprite circleSprite;
+    [SerializeField] private int graphMode;     // 0 for X-Y, 1 for Z, 2 for log
+    private GameObject graphPanel;
     private RectTransform graphContainer;
-    private static List<int> valueList;
-    private int counter;
+    private static List<int> x_y_values;
+    private static List<int> z_values;
+    private static int counter = 1;
+    private static int  logCounter = 0;
     float customTimer;
+
+    private static Text[] logs;
 
     private void Awake() {
 
-        customTimer = Time.fixedTime + 1.0f;
-        counter = 1;
+        if (graphMode == 2)
+        {
+            logs = new Text[8];
 
-        graphContainer = gameObject.GetComponent<RectTransform>();      
+            Text text1 = GameObject.Find("Log1").GetComponent<Text>();
+            text1.text = "Mode: Test";
+            text1 = GameObject.Find("Log2").GetComponent<Text>();
+            logs[0] = text1;
+            text1 = GameObject.Find("Log3").GetComponent<Text>();
+            logs[1] = text1;
+            text1 = GameObject.Find("Log3").GetComponent<Text>();
+            logs[2] = text1;
+            text1 = GameObject.Find("Log4").GetComponent<Text>();
+            logs[3] = text1;
+            text1 = GameObject.Find("Log5").GetComponent<Text>();
+            logs[4] = text1;
+            text1 = GameObject.Find("Log6").GetComponent<Text>();
+            logs[5] = text1;
+            text1 = GameObject.Find("Log7").GetComponent<Text>();
+            logs[6] = text1;
+            text1 = GameObject.Find("Log8").GetComponent<Text>();
+            logs[7] = text1;
+        }
 
-        valueList = new List<int>() { 5, 15, 30, 45, 30, 22, 17, 15, 13, 17, 25, 37, 40, 36, 33 , 11 , 22, 35}; // Initial values to process in graph
+        customTimer = Time.fixedTime;
+        if (graphMode == 0) // X-Y Mode
+        {
+            graphPanel = GameObject.Find("X-Y-Graphic");
+        }
+        else if(graphMode == 1) // Z mode
+        {
+            graphPanel = GameObject.Find("Z-Graphic");
+        }
+
+        if(graphMode == 0 || graphMode == 1)
+        {
+            graphContainer = graphPanel.GetComponent<RectTransform>();
+        }
+        x_y_values = new List<int>() { 5, 15, 30, 45, 30, 22, 17, 15, 13, 17, 25, 37, 40, 36, 33, 11, 22, 35 }; // Initial values to process in graph
+        z_values = new List<int>() { 0, 0, 10, 20, 30, 20, 10, 0, 10, 20, 30, 40, 50, 60, 70, 60, 50, 40, 30, 20, 10, 0, 10, 20, 10, 0, 10, 20, 30, 40, 30, 20, 10, 0 };
+
     }
 
     private void FixedUpdate()
     {
-        if(Time.fixedTime >= customTimer) // Do the job inside this condition once in a second.
+        if (Time.fixedTime >= customTimer) // Do the job inside this condition once in a second.
         {
-            if (counter != valueList.Count)
+            Debug.Log("Counter " + counter);
+            if (counter != x_y_values.Count)
                 counter++;
-            else
+
+            if(counter == x_y_values.Count)
             {
+                int backup;
                 if (GameObject.FindGameObjectsWithTag("mortal").Length > 0) // This condition kills the previous graph nodes and connections.
                 {
                     GameObject[] mortals = GameObject.FindGameObjectsWithTag("mortal");
                     foreach (GameObject mortal in mortals)
-                        GameObject.Destroy(mortal);
-                }   
-                for (int i = 1; i < valueList.Count; i++)   // This condition shifts the graph when the panel is full.
-                {
-                    valueList[i - 1] = valueList[i];
+                    GameObject.Destroy(mortal);
                 }
-                valueList[valueList.Count - 1] = UnityEngine.Random.Range(1, 50);   // Assigns random value to the last item for simulation purposes.
+                backup = z_values[0];
+                for (int i = 1; i < z_values.Count; i++)   // This condition shifts the graph when the panel is full.
+                {
+                    z_values[i - 1] = z_values[i];
+                }
+                z_values[z_values.Count - 1] = backup;   // Assigns random value to the last item for simulation purposes.
+                for (int i = 1; i < x_y_values.Count; i++)   // This condition shifts the graph when the panel is full.
+                {
+                    x_y_values[i - 1] = x_y_values[i];
+                }
+                x_y_values[x_y_values.Count - 1] = UnityEngine.Random.Range(1, 50);   // Assigns random value to the last item for simulation purposes.
             }
-            ShowGraph(valueList);
+
+            if (graphMode == 0) // X-Y Mode
+                ShowGraph(x_y_values);
+            else if (graphMode == 1) // Z Mode
+                ShowGraph(z_values);
+
+            if (graphMode == 2)
+            {
+                int hour;
+                int minutes;
+                int seconds;
+                hour = System.DateTime.Now.Hour;
+                minutes = System.DateTime.Now.Minute;
+                seconds = System.DateTime.Now.Second;
+                
+
+                
+                if(logCounter == 8)
+                {
+                    for(int i = 1; i < 8; i++)
+                    {
+                        logs[i - 1].text = logs[i].text;
+                     }
+                    logs[7].text = hour + ":" + minutes + ":" + seconds;
+                    logs[7].text += " X = ? " + "Y = " + x_y_values[counter - 1] + " Z = " + z_values[counter - 1];
+                }
+                else
+                {
+                    logs[logCounter].text = hour + ":" + minutes + ":" + seconds;
+                    logs[logCounter].text += " X = ? " + "Y = " + x_y_values[counter - 1] + " Z = " + z_values[counter - 1];
+                    logCounter++;
+                }
+                
+                
+                
+            }
+
+            
+
             customTimer = Time.fixedTime + 1.0f;
         }
     }
-
 
     private GameObject CreateCircle(Vector2 anchoredPosition) {         // Draws a node on the graph.
         GameObject gameObject = new GameObject("circle", typeof(Image));
@@ -62,7 +149,7 @@ public class Window_Graph : MonoBehaviour {
         return gameObject;
     }
 
-    private void ShowGraph(List<int> valueList) {                       // Displays the graph.
+    private void ShowGraph(List<int> values) {                       // Displays the graph.
 
         GameObject circle = GameObject.Find("circle");
         GameObject connection = GameObject.Find("dotConnection");
@@ -74,7 +161,7 @@ public class Window_Graph : MonoBehaviour {
         GameObject lastCircleGameObject = null;
         for (int i = 0; i < counter; i++) {
             float xPosition = xSize + i * xSize;
-            float yPosition = 10 + (valueList[i] / yMaximum) * graphHeight;
+            float yPosition = 20 + (values[i] / yMaximum) * graphHeight;
             GameObject circleGameObject = CreateCircle(new Vector2(xPosition, yPosition));
             if (lastCircleGameObject != null) {
                 CreateDotConnection(lastCircleGameObject.GetComponent<RectTransform>().anchoredPosition, circleGameObject.GetComponent<RectTransform>().anchoredPosition);
